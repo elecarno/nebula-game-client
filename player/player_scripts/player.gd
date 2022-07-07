134dd6a3b6fd07a95777e3a8e615eace11a1450a
Benjamin McEwan <54546385+elecarno@users.noreply.github.com>
2022-07-07 11:13:21 +0100

    character rework (again again again)

--- player/player_scripts/player.gd
+++ player/player_scripts/player.gd
@@ -5,10 +5,12 @@ export var enabled = true
 onready var cam = get_node("cam2d")
 onready var intr_cast = get_node("player_body/body/frontarm/intr_cast")
 onready var frontarm = get_node("player_body/body/frontarm")
+onready var rightarm = get_node("player_body/body/rightarm")
 onready var body = get_node("player_body/body")
 onready var head = get_node("player_body/head")
 onready var item = get_node("player_body/body/frontarm/held_item")
 onready var anim = get_node("player_body/anim_player")
+onready var armanim = get_node("player_body/rightarm_anim")
 onready var proj_spawn = get_node("player_body/body/frontarm/proj_spawn")
 onready var bullet = preload("res://player/bullet.tscn")
 
@@ -25,9 +27,7 @@ func _ready():
 	anim.play("idle_down")
 	set_physics_process(false)
 
-func _physics_process(delta):
-	server.fetch_playerstats() # should find way to only run once
-	
+func _physics_process(delta):	
 	# control switching
 	if Input.is_action_just_released("action"):
 		if enabled:
@@ -41,37 +41,47 @@ func _physics_process(delta):
 	var move_vec = Vector2()
 	
 	if gravity and enabled:
+		if Input.is_action_just_released("move_up"):
+			anim.play("idle_up")
+			armanim.stop()
+			rightarm.visible = false
+			frontarm.visible = true
+		if Input.is_action_just_released("move_down"):
+			anim.play("idle_down")
+			armanim.stop()
+			rightarm.visible = false
+			frontarm.visible = true
+		if Input.is_action_just_released("move_left"):
+			anim.play("idle_left")
+			armanim.stop()
+			rightarm.visible = false
+			frontarm.visible = true
+		if Input.is_action_just_released("move_right"):
+			anim.play("idle_right")
+			armanim.stop()
+			rightarm.visible = false
+			frontarm.visible = true
 		if Input.is_action_pressed("move_up"):
 			move_vec.y -=1
-			anim.play("idle_up")
-			frontarm.position = Vector2(1.5, -4.5)
-			frontarm.show_behind_parent = true
-			head_look_offset = PI/2 * -1
-			item.set_flip_v(true)
+			configure_character("walk_up", "arm_walk_up", Vector2(1.5, -4.5), true, PI/2, true, true)
 		if Input.is_action_pressed("move_down"):
 			move_vec.y +=1
-			anim.play("idle_down")
-			frontarm.position = Vector2(-2.5, -4.5)
-			frontarm.show_behind_parent = false
-			head_look_offset = PI/2
-			item.set_flip_v(false)
+			configure_character("walk_down", "arm_walk_down", Vector2(-2.5, -4.5), false, PI/2, false, true)
 		if Input.is_action_pressed("move_left"):
 			move_vec.x -=1
-			anim.play("idle_left")
-			frontarm.show_behind_parent = true
-			frontarm.position = Vector2(-0.5, -4.5)
-			head_look_offset = 0
-			item.set_flip_v(true)
+			configure_character("walk_left", "arm_walk_left", Vector2(-2.5, -4.5), true, 0, true, true)
 		if Input.is_action_pressed("move_right"):
 			move_vec.x +=1
-			anim.play("idle_right")
-			frontarm.position = Vector2(-0.5, -4.5)
-			frontarm.show_behind_parent = false
-			head_look_offset = 0
-			item.set_flip_v(false)
+			configure_character("walk_right", "arm_walk_right", Vector2(-2.5, -4.5), false, 0, false, true)
 	
 		# probably a better way to do this
 		if Input.is_action_pressed("move_up") or Input.is_action_pressed("move_down") or Input.is_action_pressed("move_right") or Input.is_action_pressed("move_left"):
+			if Input.is_action_pressed("rmb"):
+				rightarm.visible = false
+				frontarm.visible = true
+			else:
+				rightarm.visible = true
+				frontarm.visible = false
 			walking = true
 		else:
 			walking = false
@@ -118,6 +128,15 @@ func _physics_process(delta):
 			
 	# player syncronisation
 	define_player_state()
+	
+func configure_character(anima, armanima, armpos, armshow, lookoff, itemflip, rightvis):
+	anim.play(anima)
+	armanim.play(armanima)
+	frontarm.position = armpos
+	frontarm.show_behind_parent = armshow
+	head_look_offset = lookoff
+	item.set_flip_v(itemflip)
+	rightarm.visible = rightvis
 
 func _on_area2d_body_entered(body):
 	if "gravity_area" in body.name:
